869e7530d1a28de611c625d3969679bddce3418b
Folka2134 <folka2134@gmail.com>
2023-12-09 16:59:53 +0000

    setup site theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: "#dbd8c7",
+        background: "#131311",
+        primary: "#903e3c",
+        secondary: "#90903c",
+        accent: "#523c68",
+      },
+      colors2: {
+        text: "#dbd8c7",
+        background: "#131311",
+        primary: "#050505",
+        secondary: "#939389",
+        accent: "#FFC8C7",
+      },
+    },
   },
   plugins: [],
-}
+};
